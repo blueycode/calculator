1771bb5566dae789f8844a300f355dac5d685b25
Bluey Code <blueycode@gmail.com>
2023-02-27 22:42:04 -0300

    feat: develop theme switched

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const btnNumbers = document.querySelectorAll(".number");
+const changeTheme = document.querySelector("#change-theme");
 const numbers = document.querySelector("#numbers");
 const panel = document.querySelector("#panel");
 const backspace = document.querySelector(".backspace");
@@ -12,6 +13,13 @@ const subtract = document.querySelector(".subtract");
 const divide = document.querySelector(".divide");
 const multiply = document.querySelector(".multiply");
 const equals = document.querySelector(".equals");
+
+const themes = [
+    { background: "#FADDB7", foreground: "#F5B461", mainText: "#F19927" },
+    { background: "#FAB7BE", foreground: "#F67280", mainText: "#F45768" },
+    { background: "#B3D8FF", foreground: "#6EB6FF", mainText: "#4CA5FF" },
+    { background: "#FFC2B2", foreground: "#FF8E71", mainText: "#FF481A" },
+];
 
 let res = 0;
 
@@ -79,4 +87,21 @@ clear.addEventListener("click", (e) => {
 
 clearPanel.addEventListener("click", (e) => {
     panel.innerText = "0";
+});
+
+// This function will randomize between four themes that I chose...
+changeTheme.addEventListener("click", (e) => {
+    // Randomizing a random theme from "themes" array
+    const random = themes[Math.floor(Math.random() * themes.length)];
+
+    // Applying the randomized theme to our calculator:
+    document.querySelector("#calculator")
+        .style.backgroundColor = random.background;
+    document.querySelector("#top")
+        .style.color = random.foreground;
+    document.querySelectorAll(".btn").forEach((e) => {
+        e.style.backgroundColor = random.foreground;
+    });
+    numbers.style.color = random.mainText;
+    panel.style.color = random.mainText;
 });
